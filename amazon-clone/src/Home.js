bdfcc237a56ba07d5f67d3aef20c13c77420dd1e
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-09 14:08:18 -0500

    Add image in the top and create product component.

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,1 +1,31 @@
-<h1>Hello From Home Component</h1>
+import React from 'react'
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className = "home">
+            <div className = "home__container">
+                <img 
+                className = "home__image"
+                src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg" alt="Image" />
+            </div>
+
+            <div className="home__row">
+                {/*Product Component*/}
+                {/*Product Component*/}
+            </div>
+
+            <div className="home__row">
+                {/*Product Component*/}
+                {/*Product Component*/}
+                {/*Product Component*/}
+            </div>
+
+            <div className="home__row">
+                {/*Product Component*/}
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ amazon-clone/src/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Product() {
+    return (
+        <div>
+            <h1>Product Component</h1>
+        </div>
+    )
+}
+
+export default Product
