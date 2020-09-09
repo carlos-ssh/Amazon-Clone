8254e44d5bddd6115df713bcaa9e7536492d87ca
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-09 11:48:56 -0500

    Add Firebase Tools and SDK Snippet.

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCiu0rSk7rWNfEUon-Ybpp0pUhv5ENQmq4",
+    authDomain: "by-carlos-ssh.firebaseapp.com",
+    databaseURL: "https://by-carlos-ssh.firebaseio.com",
+    projectId: "by-carlos-ssh",
+    storageBucket: "by-carlos-ssh.appspot.com",
+    messagingSenderId: "579202739301",
+    appId: "1:579202739301:web:870d745d24e7165d54aa8c",
+    measurementId: "G-QQC0JGYJYZ"
+  };
